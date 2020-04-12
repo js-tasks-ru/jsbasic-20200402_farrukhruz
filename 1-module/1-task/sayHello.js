c9f5f92a5d420f393a5b19d76e9ec8220b9d4a89
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-12 21:43:10 +0300

    SayHello
    Please get my attached files of task sayHello.

---
+++ 1-module/1-task/sayHello.js
@@ -0,0 +1,33 @@
+/**
+ * Эту функцию трогать не нужно
+ */
+function print(text) {
+    console.log(text);
+  }
+  
+  /**
+   * Эту функцию нужно поменять так,
+   * чтобы функция sayHello работала корректно
+   */
+  function isValid(userName) {
+    if (userName === '' || userName.includes(',') || userName.length > 4){
+return userName;
+   }
+}
+//isValid ();
+  /**
+   * Эту функцию трогать не нужно
+   */
+  function sayHello() {
+    const userName = prompt('Введите ваше имя');
+  
+    if (isValid(userName)) {
+      print(`Welcome back, ${userName}!`);
+    } else {
+      print('Некорректное имя');
+    }
+  }
+  
+  sayHello('user');
+
+   
