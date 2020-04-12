b20da78404f920abd34debf1a934b9b8e0e7e0c6
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-12 21:54:52 +0300

    ucFirstCharBig
    Could you please get my attached files of task ucFirst

---
+++ 1-module/1-task/ucFirst.js
@@ -0,0 +1,6 @@
+function ucFirst(str){
+    str = prompt ('Какую слово изменит букву ','');
+    if (!str) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);    
+}
+document.write(ucFirst());
