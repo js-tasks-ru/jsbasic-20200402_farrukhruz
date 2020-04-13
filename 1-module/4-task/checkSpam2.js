20e112c36895a7d4dcd9eb7687b6ebc11d2a0262
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-13 09:49:39 +0300

    CheckSpam2
    For your info.

---
+++ 1-module/4-task/checkSpam2.js
@@ -0,0 +1,10 @@
+function checkSpam(str){
+    str = prompt ('Есть ли совпадение на СПАМ ?' ,'');
+    str = str.toLowerCase();
+    if (str.includes('1xbet')||str.includes('xxx')) { 
+        return 'Есть совподение';
+    } else {
+        return 'Нет совподение';
+    }      
+}
+document.write(checkSpam())
