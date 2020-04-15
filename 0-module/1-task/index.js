97da4f1c25dd8247c58df9e7645e7ae30d16f964
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-16 00:35:19 +0300

    First task.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,13 +1,7 @@
- /**
- * Power
- * @param {number} m base
- * @param {number} n index
- * @returns {number}
- */
 function sum(m, n) {
   // ваш код...
   m = prompt('Enter some number for m ?')
   n = prompt('Enter some number for n ?')
   return m * n;
 }
-console.log(sum())
+alert(sum())
