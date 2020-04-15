2be88b44398a8848641f4aa2910ff7d0337e4266
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-16 00:23:27 +0300

    Factorial.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,15 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-  // ваш код...
+function factorial( n = prompt('На какое число Факториал ? ', '')){
+if (n < 0){
+    return -1;
+}else if (n === 0){
+    return 1;
+}else {
+    return n * factorial(n-1)
+}
+
+}    
+alert (factorial ());  
 }
