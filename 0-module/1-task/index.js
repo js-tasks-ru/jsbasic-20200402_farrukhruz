c52a7a0432c0f73127c5f60501e46a5318232670
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-15 23:13:15 +0300

    index.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * Power
  * @param {number} m base
  * @param {number} n index
@@ -6,4 +6,8 @@
  */
 function sum(m, n) {
   // ваш код...
+  m = prompt('Enter some number for m ?')
+  n = prompt('Enter some number for n ?')
+  return m * n;
 }
+console.log(sum())
