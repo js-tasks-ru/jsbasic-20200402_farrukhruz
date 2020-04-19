83253869cfb090251374d93f5db5e0001515efdc
farrukhruz.kiyomov <farrukhruz@mail.ru>
2020-04-19 09:56:58 +0300

    Факториал

---
+++ 1-module/1-task/factorial.js
@@ -0,0 +1,12 @@
+function factorial( n = prompt('На какое число Факториал ? ', '')){
+  if (n < 0){
+      return -1;
+  }else if (n === 0){
+      return 1;
+  }else {
+      return n * factorial(n-1)
+  }
+  
+  }    
+  alert (factorial ());
+  }
