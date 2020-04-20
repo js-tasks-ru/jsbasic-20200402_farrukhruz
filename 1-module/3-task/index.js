51c6f64f7b4ba87bf760e7fd54661ac63d21e3d7
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-20 19:05:02 +0300

    Update index.js

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,6 +1,4 @@
 function ucFirst(str){
-  str = prompt ('Какую слово изменит букву ','');
   if (!str) return str;
   return str.charAt(0).toUpperCase() + str.slice(1);    
 }
-document.write(ucFirst());
