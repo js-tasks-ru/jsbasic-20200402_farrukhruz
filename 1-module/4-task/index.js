5815c4706cb35b787aa73829798ee5769cec704e
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-20 19:11:43 +0300

    Update index.js

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,10 +1,8 @@
 function checkSpam(str){
-  str = prompt ('Есть ли совпадение на СПАМ ?' ,'');
   str = str.toLowerCase();
   if (str.includes('1xbet')||str.includes('xxx')) { 
-      return 'Есть совподение';
+  return true
   } else {
-      return 'Нет совподение';
-  }      
+  return false
+  }
 }
-document.write(checkSpam())
