9fae146ca854f5fd20bad14d1949ae19ceebf37a
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-20 19:32:54 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,12 +1,11 @@
-function factorial(n)){
-  if (n < 0){
-      return -1;
-  }else if (n === 0){
-      return 1;
-  }else {
-      return n * factorial(n-1)
-  }
-  
-  }    
-  alert (factorial ());
-  }
+function factorial(n) {
+    if (n < 0){
+        return -1;
+    }else if (n === 0){
+        return 1;
+    }else {
+        return n * factorial(n-1)
+    }
+    
+    }    
+   factorial ();
