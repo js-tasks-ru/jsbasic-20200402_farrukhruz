ed8b87ac0b948bada0d6a6470343655e3a2a8846
farrukhruz.kiyomov <farrukhruz@mail.ru>
2020-04-22 18:11:26 +0300

    Calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,14 @@
 let calculator = {
-  // ваш код
-};
-
+  sum() {
+    return calculator.a + calculator.b
+    },
+  mul() {
+    return calculator.a * calculator.b
+    },
+  read() {
+    calculator.a = parseFloat(prompt(`Добавте a `, 0))
+    calculator.b = parseFloat(prompt(`Добавте б `, 0))
+    },
+    };
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
