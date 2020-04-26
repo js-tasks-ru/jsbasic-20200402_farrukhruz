272960479a1557eb3134966bb7ed99a3b7ba73f7
farrukhruz.kiyomov <farrukhruz@mail.ru>
2020-04-26 10:13:39 +0300

    1m2task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,31 +1,26 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
   console.log(text);
 }
-
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(userName) {
-  if (!userName === '' || userName.includes(',') || userName.length > 4){
-return userName;
- }
+function isValid(name) {
+  if (name ===' '|| !name.length > 4 || name.length === 4){
+return true
+ } 
+return false
 }
-//isValid ();
+
 /**
  * Эту функцию трогать не нужно
  */
 function sayHello() {
   const userName = prompt('Введите ваше имя');
-
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
     print('Некорректное имя');
   }
 }
-
- 
+sayHello('user');
