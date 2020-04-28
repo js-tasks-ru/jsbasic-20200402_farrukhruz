882adc1003ffa3660b8d2d0cab39daba356b2e97
Farrukhruz Kiyomov <farrukhruz@mail.ru>
2020-04-28 17:16:34 +0300

    3m3task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -15,12 +15,3 @@ userNames.push(user.name)
   return userNames;
 }
 namify(users); // ['Вася', 'Петя', 'Маша']
-
-
-function namify(users) {
-  let userNames =[];
- for (let user of users){
- userNames.push(user.name)
- }
-   return userNames;
- }
